25a968a998f14205c2dfe289ebf0b4e4e8687114
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-04-03 09:26:48 -0400

    added variables to start game

---
+++ TODO notes.js
@@ -0,0 +1,23 @@
+//TODO
+/**
+ * make a way for user to view question on card
+ * allow input for answer
+ * add functionality and enable SUBMIT and PASS button
+ *  !submit
+ *    this is what happens I hit submit
+ *    check the input to see if it matched the question answer,if else statement (if yes grant point and ask them to pick another card, else give feedback of wrong subtract pointsand goes to player 2 )
+ *    if pick another card repeat from first step
+ * !Pass
+ *  on click pass switch player turn, and ask other player to answer the question, if
+ * ! after submit or pass
+ *  make functionality for player two to answer question (give them submit or pass functionality)
+ *  continue until player has total points or board has been cleared
+ * !Round Two
+ * add above function to this page as well
+ * ! Final Round
+ * ask both users to wager
+ *  display one question before checking answer get input from both users
+ *  check both answers and all right get awarded their wager if wrong they lose wager and points
+ * Say who one
+ *
+ */
